4ffec6218352a81cf9e4a4f395041459dc2a84f8
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2023-12-20 13:02:55 +0100

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,12 @@
 print("Hello World, Hola Mundo. Hola, Night City")
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://root:example@mongo:27017")
+db=client.testdb
+
+try: db.command("ServerStatus")
+except Exception as e:print(e)
+else: print("HAS ENTRADO EN LA MONGO")
+
+client.close()
