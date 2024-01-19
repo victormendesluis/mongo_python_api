978b9a146face17275113cc7296d6d8364781156
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-19 14:20:47 +0100

    Update app.py

--- api/src/app.py
+++ api/src/app.py
@@ -9,4 +9,27 @@ try: db.command("ServerStatus")
 except Exception as e:print(e)
 else: print("HAS ENTRADO EN LA MONGO")
 
+result=db.createCollection("contacts", {
+   validator: {$jsonSchema: {
+      bsonType: "object",
+      required: ["phone"],
+      properties: {
+         phone: {
+            bsonType: "string",
+            description: "must be a string and is required"
+         },
+         email: {
+            bsonType: "string",
+            pattern: "@mongodb\.com$",
+            description: "must be a string and match the regular expression pattern"
+         },
+         status: {
+            enum: [ "Unknown", "Incomplete" ],
+            description: "can only be one of the enum values"
+         }
+      }
+   }}
+})
+print(result)
+
 client.close()
