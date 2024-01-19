9be118f14e00bcea885c599290b1c65016ee9965
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-19 15:00:39 +0100

    Create todo.py

---
+++ api/src/todo.py
@@ -0,0 +1,7 @@
+class todo:
+  def __init__(self, titulo, descripcion, fecha, terminado, tipo):
+        self._titulo = titulo
+        self._descripcion = descripcion
+        self._fecha = fecha
+        self._terminado = terminado
+        self._tipo = tipo
