7b8c636a7d8858fa0549c9acba91ef73e07aed1d
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-19 15:04:02 +0100

    Update main.py

--- api/src/main.py
+++ api/src/main.py
@@ -1,3 +1,14 @@
+from pymongo import MongoClient
+
+print('Ejecutando python en el contenedor')
+#MongoDB
+client = MongoClient("mongodb://root:example@mongo:27017")
+db=client.apiDB
+
+try: db.command("ServerStatus")
+except Exception as e:print(e)
+else: print("HAS ENTRADO EN LA MONGO")
+
 def print_options():
     print('LISTA DE TAREAS')
     print('*' * 50)
