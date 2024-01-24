d513984b4f1220851ff9c406293e32bda498eccf
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-24 11:33:08 +0100

    Update app.py

--- api/src/app.py
+++ api/src/app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 print('Ejecutando python en el contenedor')
 #MongoDB
 client = MongoClient("mongodb://root:example@mongo:27017")
-db=client.apiDB
+db=client.testdb
 print('Bienvenido a pymongo')
 try: db.command("ServerStatus")
 except Exception as e:print(e)
