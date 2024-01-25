8b52be929b07cdec7b0dd95500436914d1bd10b2
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-25 18:25:38 +0100

    Update conexion.py

--- api/src/conexion.py
+++ api/src/conexion.py
@@ -1,5 +1,10 @@
 class conexion:
   def __init__(self, user, password, host, port):
     self.client=MongoClient(f"mongodb://{user}:{password}@{host}:{port}/")
-  def cerrar_conexion(self)
+  
+  def cerrar_conexion(self):
     self.client.close()
+
+  def traer_bbdd(self):
+    return client.apidb
+  
