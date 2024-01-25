b148e5a4140b243a17f9c8b5d5f86d563e98750d
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-25 16:46:18 +0100

    Update main.py

--- api/src/main.py
+++ api/src/main.py
@@ -7,6 +7,27 @@ db=client.apidb
 
 try: db.command("serverStatus")
 except Exception as e:print(e)
-else: print("HAS ENTRADO EN LA MONGO")
+else: print("You have access to mongodb")
 
-def print_options():
+def create_collection():
+  db.createCollection("todo", {
+   validator: {$jsonSchema: {
+      bsonType: "object",
+      required: ["phone"],
+      properties: {
+         phone: {
+            bsonType: "string",
+            description: "must be a string and is required"
+         },
+         email: {
+            bsonType: "string",
+            pattern: "@mongodb\.com$",
+            description: "must be a string and match the regular expression pattern"
+         },
+         status: {
+            enum: [ "Unknown", "Incomplete" ],
+            description: "can only be one of the enum values"
+         }
+      }
+   }}
+})
