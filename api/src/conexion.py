46fc646abc20056a8038e5f7f6241caf0ac7eea3
victormendesluis <149801926+victormendesluis@users.noreply.github.com>
2024-01-25 18:39:32 +0100

    Update conexion.py

--- api/src/conexion.py
+++ api/src/conexion.py
@@ -1,10 +1,17 @@
+from pymongo import MongoClient
+
 class MiConexion:
-  def __init__(self, user, password, host, port):
-    self.client=MongoClient(f"mongodb://{user}:{password}@{host}:{port}/")
-  
+  def __init__(self, user, password, host, port, client):
+    try:
+      # Establece la conexión a la base de datos
+      self.client=MongoClient(f"mongodb://{user}:{password}@{host}:{port}/")
+      print("Conexión exitosa a la base de datos.")
+    except Exception as e:
+      print(f"Error al conectar a la base de datos: {e}")
+    
   def cerrar_conexion(self):
     self.client.close()
 
   def traer_bbdd(self):
-    return client.apidb
+    return self.client.apidb
   
